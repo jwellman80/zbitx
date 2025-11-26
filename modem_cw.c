434c6bb6e5df500ad4241be07a785b6fc6daf5e4
Jared Wellman <jwellman@gmail.com>
2025-11-25 22:47:49 -0600

    Iambic B Fix.

--- modem_cw.c
+++ modem_cw.c
@@ -481,7 +481,7 @@ float cw_tx_get_sample(){
 				cw_next_symbol = CW_DASH;
 			}
 			if (cw_next_symbol == CW_IDLE && cw_last_symbol == CW_DASH && (symbol_now & CW_DOT)){
-				cw_next_symbol = CW_DASH;
+				cw_next_symbol = CW_DOT;
 			}
 		}
 		break;
